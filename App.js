3b927dd29bb4d28575ed813a1b0f686616b0cd5f
michaelmcgrath <mmmcgrath5@hotmail.com>
2016-02-02 10:18:31 +1300

    stuff

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class App extends React.component{
+  render(){
+    return <div>Hello</div>
+  }
+}
+
+export default App
---
+++ main.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App form './App'
+ReactDom.render(<App />, document.getElementById('app'))
