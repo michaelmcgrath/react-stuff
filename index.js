977def86cc54d1eba624083e561e21d52d6f4d05
Michael McGrath <email": null>
2016-02-03 12:16:05 +1300

    get it done son

---
+++ index.js
@@ -0,0 +1,6 @@
+import React, {component} from 'react'
+import ReactDOM from 'react-dom'
+
+const mountNode = document.getElementById('App')
+
+ReactDOM.render(<helloMessage name="Michael" />, mountNode)
